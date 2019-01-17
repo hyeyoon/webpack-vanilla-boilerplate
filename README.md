40e91d2b8b0c4010f01dbe65e0219131f3c7658d
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2019-01-17 14:59:36 +0900

    Add boilerplate

---
+++ README.md
@@ -0,0 +1,16 @@
+# Webpack Simple Boilerplate
+
+> Simple webpack4 boilerplate with Sass, Babel.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:8080
+$ yarn run dev
+
+# build for production
+$ yarn run build
+```
---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  // devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]',
+          limit: 10000,
+        },
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ]
+}
